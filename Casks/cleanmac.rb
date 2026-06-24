d3887e0e2b50720291256c960c70e64341da7b29
Alex1986-rgb <139079561+Alex1986-rgb@users.noreply.github.com>
2026-06-24 15:43:01 +0300

    cleanmac 2.37.0

--- Casks/cleanmac.rb
+++ Casks/cleanmac.rb
@@ -1,6 +1,6 @@
 cask "cleanmac" do
-  version "2.36.0"
-  sha256 "efa0f9844f137f9c4f85537fee2ea6ce0db8987a725c16585e183ca391cd9e7b"
+  version "2.37.0"
+  sha256 "b8a27458c9c7fb500129e4d48adc4cd0ed63b67dce96bca26c08a45943d2e320"
 
   url "https://github.com/Alex1986-rgb/CleanMac/releases/download/v#{version}/CleanMac.dmg"
   name "CleanMac"
