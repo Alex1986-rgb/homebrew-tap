906f66fdeeb45e9afb9bf8315156c3c9a606369d
Alex1986-rgb <139079561+Alex1986-rgb@users.noreply.github.com>
2026-06-24 09:52:44 +0300

    cleanmac 2.29.0: редизайн в стиле Apple/iOS

--- Casks/cleanmac.rb
+++ Casks/cleanmac.rb
@@ -1,6 +1,6 @@
 cask "cleanmac" do
-  version "2.28.0"
-  sha256 "ce1601842dc0d9412b5863527a0d7e7f9621f5d143360c6a4a586b4f068e5174"
+  version "2.29.0"
+  sha256 "c5e8dbedba12f9d3f77a25c26fcc679f791566a842550dd645a1de2670256c67"
 
   url "https://github.com/Alex1986-rgb/CleanMac/releases/download/v#{version}/CleanMac.dmg"
   name "CleanMac"
