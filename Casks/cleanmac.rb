81141b5d1496a26bb0ecf08461bbeb1b2dd5151a
Alex1986-rgb <139079561+Alex1986-rgb@users.noreply.github.com>
2026-06-24 15:24:16 +0300

    cleanmac 2.35.0: дашборд-HUD на один экран

--- Casks/cleanmac.rb
+++ Casks/cleanmac.rb
@@ -1,6 +1,6 @@
 cask "cleanmac" do
-  version "2.32.0"
-  sha256 "0bae87eb2b518d0e5888562ea661140a17a1546074c4814a7664e498705eb06f"
+  version "2.35.0"
+  sha256 "246b6c139bca6d3657e8fc29b6cf30f26d258bedae9908fe614e4ab664e25bf7"
 
   url "https://github.com/Alex1986-rgb/CleanMac/releases/download/v#{version}/CleanMac.dmg"
   name "CleanMac"
